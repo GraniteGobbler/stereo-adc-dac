5e3de5c4dd08cef1fe060aa0a49f5d9821059c51
GraniteGobbler <chriskois4@gmail.com>
2024-03-08 14:56:53 +0100

    Working encoders, needs separation

---
+++ test-codes/encoder-test/main/main.c
@@ -0,0 +1,93 @@
+#include <inttypes.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <string.h>
+#include <encoder.h>
+#include <esp_idf_lib_helpers.h>
+#include <esp_log.h>
+
+// Connect common encoder pin to ground
+#if HELPER_TARGET_IS_ESP8266
+#define RE1_A_GPIO   14
+#define RE1_B_GPIO   12
+#define RE1_BTN_GPIO 13
+
+#elif HELPER_TARGET_IS_ESP32
+#define RE1_A_GPIO   9
+#define RE1_B_GPIO   10
+#define RE1_BTN_GPIO 11
+#define RE2_A_GPIO   12
+#define RE2_B_GPIO   13
+#define RE2_BTN_GPIO 14
+
+#else
+#error Unknown platform
+#endif
+
+#define EV_QUEUE_LEN 5
+
+static const char *TAG = "encoder_example";
+
+static QueueHandle_t event_queue;
+static rotary_encoder_t re1;
+static rotary_encoder_t re2;
+
+void test(void *arg)
+{
+    // Create event queue for rotary encoders
+    event_queue = xQueueCreate(EV_QUEUE_LEN, sizeof(rotary_encoder_event_t));
+
+    // Setup rotary encoder library
+    ESP_ERROR_CHECK(rotary_encoder_init(event_queue));
+
+    // Add one encoder
+    memset(&re1, 0, sizeof(rotary_encoder_t));
+    re1.pin_a = RE1_A_GPIO;
+    re1.pin_b = RE1_B_GPIO;
+    re1.pin_btn = RE1_BTN_GPIO;
+    ESP_ERROR_CHECK(rotary_encoder_add(&re1));
+
+    // Add one encoder
+    memset(&re2, 0, sizeof(rotary_encoder_t));
+    re2.pin_a = RE2_A_GPIO;
+    re2.pin_b = RE2_B_GPIO;
+    re2.pin_btn = RE2_BTN_GPIO;
+    ESP_ERROR_CHECK(rotary_encoder_add(&re2));
+
+    rotary_encoder_event_t e;
+    int32_t val1 = 0;
+
+    ESP_LOGI(TAG, "Initial value: %" PRIi32, val1);
+    while (1)
+    {
+        xQueueReceive(event_queue, &e, portMAX_DELAY);
+
+        switch (e.type)
+        {
+            case RE_ET_BTN_PRESSED:
+                // ESP_LOGI(TAG, "Button pressed");
+                ESP_LOGI(TAG, "Button pressed from %d", re2.index);
+                break;
+            case RE_ET_BTN_RELEASED:
+                ESP_LOGI(TAG, "Button released");
+                break;
+            case RE_ET_BTN_CLICKED:
+                ESP_LOGI(TAG, "Button clicked");
+                break;
+            case RE_ET_BTN_LONG_PRESSED:
+                ESP_LOGI(TAG, "Looooong pressed button");
+                break;
+            case RE_ET_CHANGED:
+                val1 -= e.diff;
+                ESP_LOGI(TAG, "Value = %" PRIi32, val1);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void app_main()
+{
+    xTaskCreate(test, TAG, configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+}
