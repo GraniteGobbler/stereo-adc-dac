71b19aa7f1a3049649680f12a25d08cc1779ebea
GraniteGobbler <chriskois4@gmail.com>
2024-05-24 18:11:56 +0200

    Updated code formatting

--- test-codes/i2s-test/main/ParamEQ/ParamEQ.c
+++ test-codes/i2s-test/main/ParamEQ/ParamEQ.c
@@ -1,11 +1,11 @@
-#include "IFX_PeakingFilter.h"
+#include "ParamEQ.h"
 
 #define M_PI    3.14159265359   // My pi
 
-void IFX_PeakingFilter_Init(IFX_PeakingFilter *filt, float sampleRate_Hz){
+void ParamEQ_Init(ParamEQ *filt, float fs_Hz){
 
     /* Compute sample time */
-    filt->sampleTime_s = 1.0f / sampleRate_Hz;    // Same as ADC
+    filt->T_s = 1.0f / fs_Hz;    // Same as ADC
 
     /* Clear filter memory */
     for (uint8_t n = 0; n < 3; n++){
@@ -16,24 +16,23 @@ void IFX_PeakingFilter_Init(IFX_PeakingFilter *filt, float sampleRate_Hz){
     }
 
     /* Calculate default (all-pass) filter coefficients */
-    IFX_PeakingFilter_SetParameters(filt, 1.0f, 0.0f, 1.0f);
+    ParamEQ_SetParameters(filt, 1.0f, 0.0f, 1.0f);
 
 }
 
 /* Compute filter coefficients, If boostCut_linear > 1 → boost, < 1 → cut */
-void IFX_PeakingFilter_SetParameters(IFX_PeakingFilter *filt, float centerFrequency_Hz, float bandwidth_Hz, float boostCut_linear){
+void ParamEQ_SetParameters(ParamEQ *filt, float fc_Hz, float B_Hz, float g){
 
     /* Convert Hz to rad/s, pre-warp, multiply by sampling time (wc*T = 2 * tan(wc*T/2)) */
-    float wcT = 2.0f * tanf(M_PI * centerFrequency_Hz * filt->sampleTime_s);
+    float wcT = 2.0f * tanf(M_PI * fc_Hz * filt->T_s);
 
     /* Compute quality factor (Q = B / fc) */
-    // float Q = bandwidth_Hz / centerFrequency_Hz;    // ! NOT PREWARPED ! 
-    float Q = centerFrequency_Hz / bandwidth_Hz;    // ! NOT PREWARPED ! 
+    float Q = fc_Hz / B_Hz;    // ! NOT PREWARPED ! 
 
     /* Compute filter coefficients */
-    filt->a[0] = 4.0f + 2.0f * (boostCut_linear / Q) * wcT + wcT * wcT;
+    filt->a[0] = 4.0f + 2.0f * (g / Q) * wcT + wcT * wcT;
     filt->a[1] = 2.0f * wcT * wcT - 8.0f;
-    filt->a[2] = 4.0f - 2.0f * (boostCut_linear / Q) * wcT + wcT * wcT;
+    filt->a[2] = 4.0f - 2.0f * (g / Q) * wcT + wcT * wcT;
 
     filt->b[0] = 1.0f / (4.0f + 2.0f * (1.0f / Q) * wcT + wcT * wcT);    // Note: 1 / b0
     filt->b[1] = -(2.0f * wcT * wcT - 8.0f);                    // Note: -b1
@@ -42,7 +41,7 @@ void IFX_PeakingFilter_SetParameters(IFX_PeakingFilter *filt, float centerFreque
 
 
 /* 2nd order peaking filter, 3 sample input, 3 sampe output. Two samples would suffice. */
-float IFX_PeakingFilter_Update(IFX_PeakingFilter *filt, float in){
+float ParamEQ_Update(ParamEQ *filt, float in){
 
     /* Shift samples */
     filt->x[2] = filt->x[1];
@@ -58,6 +57,5 @@ float IFX_PeakingFilter_Update(IFX_PeakingFilter *filt, float in){
     
     /* Return current output sample */
     return (filt->y[0]);
-    // return (in);
-    
+
 }
--- test-codes/i2s-test/main/main.c
+++ test-codes/i2s-test/main/main.c
@@ -15,37 +15,36 @@
 #include "esp_check.h"
 #include "sdkconfig.h"
 #include "./pcm1862/pcm1862.c"
-#include "./IFX_PeakingFilter/IFX_PeakingFilter.h"
-#include "./RC_Filter/RC_Filter.h"
+#include "./ParamEQ/ParamEQ.h"
 
 static const char *TAG = "I2S TEST";
 static const char err_reason[][30] = {"input param is invalid",
                                       "operation timeout"
                                      };
 
-#define EXAMPLE_I2S_DUPLEX_MODE         CONFIG_USE_DUPLEX
+#define I2S_DUPLEX_MODE         CONFIG_USE_DUPLEX
 
-#define EXAMPLE_STD_BCLK_IO1 GPIO_NUM_15 // I2S bit clock io number
-#define EXAMPLE_STD_WS_IO1 GPIO_NUM_16   // I2S word select io number
-#define EXAMPLE_STD_DOUT_IO1 GPIO_NUM_18 // I2S data out io number
-#define EXAMPLE_STD_DIN_IO1 GPIO_NUM_17  // I2S data in io number
-#define DA_SMUTE GPIO_NUM_46
+#define BCLK        GPIO_NUM_15 // I2S bit clock io number
+#define WS          GPIO_NUM_16   // I2S word select io number
+#define DOUT        GPIO_NUM_18 // I2S data out io number
+#define DIN         GPIO_NUM_17  // I2S data in io number
+#define DA_SMUTE    GPIO_NUM_46
 #define DA_PWR_DOWN GPIO_NUM_9
-#define AMP_ENABLE GPIO_NUM_10
+#define AMP_ENABLE  GPIO_NUM_10
 
-#define SAMPLE_RATE_HZ                  96000
-#define SAMPLE_RATE_HZ_F                96000.0f
+#define SAMPLE_RATE_HZ          96000
+#define SAMPLE_RATE_HZ_F        96000.0f
 
-#define EXAMPLE_BUFF_SIZE               256
+#define BUFF_SIZE               256
 
 // #define UINT32_TO_FLOAT                 1.0f/(65536.0f)
 // #define FLOAT_TO_UINT32                 65536.0f
 
 static i2s_chan_handle_t                tx_chan;        // I2S tx channel handler
 static i2s_chan_handle_t                rx_chan;        // I2S rx channel handler
-static IFX_PeakingFilter                peak_filt_r;    // Peaking Filter struct
-static IFX_PeakingFilter                peak_filt_l;    // Peaking Filter struct
-// static RC_Filter                        lpf;      // Peaking Filter struct
+static ParamEQ                peak_filt_r;    // Peaking Filter struct
+static ParamEQ                peak_filt_l;    // Peaking Filter struct
+
 
 // Default Filter Config
 float volume                =   1.0f;
@@ -54,7 +53,10 @@ float B                     =   10.0f;
 float g                     =   1.0f;
 
 
-static esp_err_t i2s_example_init_std_duplex(void)
+
+
+
+static esp_err_t i2s_init_std_duplex(void)
 {
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_SLAVE);
     chan_cfg.auto_clear = true; // Auto clear the legacy data in the DMA buffer
@@ -65,10 +67,10 @@ static esp_err_t i2s_example_init_std_duplex(void)
         .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
             .mclk = I2S_GPIO_UNUSED,    // some codecs may require mclk signal, this example doesn't need it
-            .bclk = EXAMPLE_STD_BCLK_IO1,
-            .ws   = EXAMPLE_STD_WS_IO1,
-            .dout = EXAMPLE_STD_DOUT_IO1,
-            .din  = EXAMPLE_STD_DIN_IO1, // In duplex mode, bind output and input to a same gpio can loopback internally
+            .bclk = BCLK,
+            .ws   = WS,
+            .dout = DOUT,
+            .din  = DIN, // In duplex mode, bind output and input to a same gpio can loopback internally
             .invert_flags = {
                 .mclk_inv = false,
                 .bclk_inv = false,
@@ -89,14 +91,17 @@ static esp_err_t i2s_example_init_std_duplex(void)
 }
 
 
-static void i2s_echo(void *args)
+
+
+
+static void i2s_process(void *args)
 {
-    int32_t *filtIN_buf = malloc(EXAMPLE_BUFF_SIZE);
+    int32_t *filtIN_buf = malloc(BUFF_SIZE);
     if (!filtIN_buf) {
         ESP_LOGE(TAG, "[echo] No memory for filterIn data buffer");
         abort();
     }
-    int32_t *filtOUT_buf = malloc(EXAMPLE_BUFF_SIZE);
+    int32_t *filtOUT_buf = malloc(BUFF_SIZE);
     if (!filtOUT_buf) {
         ESP_LOGE(TAG, "[echo] No memory for filterOut data buffer");
         abort();
@@ -114,33 +119,25 @@ static void i2s_echo(void *args)
     ESP_LOGI(TAG, "[echo] Echo start");
 
     while (1) {
-        memset(filtIN_buf, 0, EXAMPLE_BUFF_SIZE);
-        memset(filtOUT_buf, 0, EXAMPLE_BUFF_SIZE);
+        memset(filtIN_buf, 0, BUFF_SIZE);
+        memset(filtOUT_buf, 0, BUFF_SIZE);
 
-        /* Read sample data from mic */
-        ret = i2s_channel_read(rx_chan, filtIN_buf, EXAMPLE_BUFF_SIZE, &bytes_read, 1000);
+        /* Read I2S data */
+        ret = i2s_channel_read(rx_chan, filtIN_buf, BUFF_SIZE, &bytes_read, 1000);
         if (ret != ESP_OK) {
             ESP_LOGE(TAG, "[echo] i2s read failed, %s", err_reason[ret == ESP_ERR_TIMEOUT]);
             abort();
         }
 
-        // filtOUT_buf = filtIN_buf;
-
-        for (int32_t n = 0; n < (EXAMPLE_BUFF_SIZE) - 1; n += 2){
-
-            /* // Convert uint_32 to float     UINT32_TO_FLOAT *  */
+        /* Apply filter */
+        for (int32_t n = 0; n < (BUFF_SIZE) - 1; n += 2){
+   
             leftIn = (float)filtIN_buf[n];
             rightIn = (float)filtIN_buf[n+1];
 
-            leftProcessed = IFX_PeakingFilter_Update(&peak_filt_l, leftIn);
-            rightProcessed = IFX_PeakingFilter_Update(&peak_filt_r, rightIn);
-            // leftProcessed = leftIn;
-            // rightProcessed = rightIn;
+            leftProcessed = ParamEQ_Update(&peak_filt_l, leftIn);
+            rightProcessed = ParamEQ_Update(&peak_filt_r, rightIn);
 
-            // leftProcessed = RC_Filter_Update(&lpf, leftIn);
-            // rightProcessed = RC_Filter_Update(&lpf, rightIn);
-
-            /* // Convert float to uint_32      FLOAT_TO_UINT32 *  */
             leftOut = (int32_t)(leftProcessed * volume);
             rightOut = (int32_t)(rightProcessed * volume);
 
@@ -148,15 +145,10 @@ static void i2s_echo(void *args)
             filtOUT_buf[n]   = leftOut;
             filtOUT_buf[n+1] = rightOut;
 
-            // printf("%lu", filtIN_buf[n]);
-            // printf("  ");
-
         }
 
-        // printf("\n");
-
-        /* Write sample data to earphone */
-        ret = i2s_channel_write(tx_chan, filtOUT_buf, EXAMPLE_BUFF_SIZE, &bytes_write, 1000);
+        /* Write I2S data */
+        ret = i2s_channel_write(tx_chan, filtOUT_buf, BUFF_SIZE, &bytes_write, 1000);
         if (ret != ESP_OK) {
             ESP_LOGE(TAG, "[echo] i2s write failed, %s", err_reason[ret == ESP_ERR_TIMEOUT]);
             abort();
@@ -165,13 +157,18 @@ static void i2s_echo(void *args)
             ESP_LOGW(TAG, "[echo] %d bytes read but only %d bytes are written", bytes_read, bytes_write);
         }
 
-
     }
     vTaskDelete(NULL);
 }
 
+
+
+
+
+
 void app_main(void)
 {
+    ESP_LOGW(TAG,"GPIO init\n");
     gpio_set_direction(DA_SMUTE, GPIO_MODE_OUTPUT);
     gpio_set_direction(DA_PWR_DOWN, GPIO_MODE_OUTPUT);
     gpio_set_direction(AMP_ENABLE, GPIO_MODE_OUTPUT);
@@ -180,27 +177,27 @@ void app_main(void)
     gpio_set_level(DA_PWR_DOWN, 1); // DA_PWR_DOWN - H
     gpio_set_level(AMP_ENABLE, 1); // AMP_ENABLE - H
 
+
     ESP_LOGW(TAG,"I2C Init\n");
     pcm1862_init();
 
+
     ESP_LOGW(TAG,"I2S Init\n");
     /* Initialize i2s peripheral */
-    if (i2s_example_init_std_duplex() != ESP_OK) {
+    if (i2s_init_std_duplex() != ESP_OK) {
         ESP_LOGE(TAG, "i2s driver init failed");
         abort();
     } else {
         ESP_LOGI(TAG, "i2s driver init success");
     }
 
+    ESP_LOGW(TAG,"ParamFilter Init\n");
+    ParamEQ_Init(&peak_filt_r, SAMPLE_RATE_HZ_F);
+    ParamEQ_Init(&peak_filt_l, SAMPLE_RATE_HZ_F);
+    ParamEQ_SetParameters(&peak_filt_r, fc, B, g);
+    ParamEQ_SetParameters(&peak_filt_l, fc, B, g);
 
-    IFX_PeakingFilter_Init(&peak_filt_r, SAMPLE_RATE_HZ_F);
-    IFX_PeakingFilter_Init(&peak_filt_l, SAMPLE_RATE_HZ_F);
-    IFX_PeakingFilter_SetParameters(&peak_filt_r, fc, B, g);
-    IFX_PeakingFilter_SetParameters(&peak_filt_l, fc, B, g);
-    // RC_Filter_Init(&lpf, fc, SAMPLE_RATE_HZ_F);
 
-
-    /* Echo the sound from MIC in echo mode */
-    xTaskCreate(i2s_echo, "i2s_echo", 8192, NULL, 5, NULL);
+    xTaskCreate(i2s_process, "i2s_process", 8192, NULL, 5, NULL);
 
 }
